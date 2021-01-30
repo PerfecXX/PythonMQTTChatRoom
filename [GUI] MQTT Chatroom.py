75967b899638ec91975ee65a59d10f44e3946546
Teeraphat Kullanankanjana <ku.teeraphat_st@tni.ac.th>
2021-01-30 14:59:38 +0700

    First version of chat room
    Feature 
    1. Send message via MQTT broker 
    2. GUI(Tkinter)
    3. No echo message from local user
    Next update
    1. Setting user data or connection can be set in menubar 
    2. Theme/Font setting

---
+++ [GUI] MQTT Chatroom.py
@@ -0,0 +1,110 @@
+from tkinter import *
+from tkinter import ttk
+import random as rd
+import string
+import paho.mqtt.client as mqtt
+
+hostname = "test.mosquitto.org"
+RoomName = "TNIRobot/"
+ConnectionStatus = False
+global DummyVar
+DummyVar = "\n"
+nickname = "FRANK"
+
+
+def on_connection(client, user_data, flag, rc):
+    global conn_status  # global variable in this file
+    status_decoder = {  # swtich case in python style using dictionary
+        0: "Successfully Connected",
+        1: "Connection refused: Incorrect Protocol Version",
+        2: "Connection refused: Invalid Client Identifier",
+        3: "Connection refused: Server Unavailable",
+        4: "Connection refused: Bad Username/Password",
+        5: "Connection refused: Not Authorized",
+    }
+    conn_text = ("System>>{} has connected to broker with status: \n\t{}.\n".format(client_id, status_decoder.get(rc)))
+    ChatFill.configure(state="normal")
+    ChatFill.insert(INSERT, str(conn_text))
+    client.subscribe(RoomName)
+    conn_status = True
+    ChatFill.configure(state="disabled")
+
+
+def on_message(client, user_data, msg):
+    # check incoming payload to prevent owner echo text
+    global incoming_massage
+    incoming_massage = msg.payload.decode("utf-8")
+    if incoming_massage.find(DummyVar) >= 0:
+        pass
+    else:
+        ChatFill.configure(state="normal")
+        ChatFill.insert(INSERT, str(incoming_massage))
+        ChatFill.configure(state="disabled")
+
+
+def send_message():
+    global DummyVar
+    get_message = str(MassageFill.get("1.0", END))
+    if get_message == " ":
+        pass
+    else:
+        send_message = "\n{}>>\t{}".format(nickname, get_message)
+        DummyVar = send_message
+        ChatFill.configure(state="normal")
+        client.publish(RoomName, send_message)
+        ChatFill.insert(INSERT, str(send_message))
+        MassageFill.delete("1.0", END)
+        ChatFill.configure(state="disabled")
+
+
+window = Tk()
+window.title("Chat room")
+window.minsize(600, 400)
+window.resizable(0, 0)
+
+Frame1 = LabelFrame(window, text="Chat Window", width=600, height=300)
+Frame1.place(y=0, x=0)
+Frame2 = LabelFrame(window, text="Enter Massage", width=600, height=100)
+Frame2.place(y=300, x=0)
+
+YChatFillScroll = Scrollbar(Frame1)
+YChatFillScroll.place(y=0, x=550, height=250)
+XChatFillScroll = Scrollbar(Frame1, orient=HORIZONTAL)
+XChatFillScroll.place(y=251, x=0, width=550)
+
+ChatFill = Text(Frame1, yscrollcommand=YChatFillScroll.set, xscrollcommand=XChatFillScroll.set)
+ChatFill.place(x=0, y=0, width=550, height=250)
+ChatFill.configure(state="disabled")
+YChatFillScroll.config(command=ChatFill.yview)
+XChatFillScroll.config(command=ChatFill.xview)
+
+MassageFill = Text(Frame2, font=("", 16))
+MassageFill.place(x=0, y=0, width=475, height=75)
+
+SendButton = Button(Frame2, text="Send", command=send_message)
+SendButton.place(x=480, y=0, width=100, height=75)
+
+WindowMenu = Menu(window)
+FileMenuBar = Menu(WindowMenu, tearoff=0)
+FileMenuBar.add_command(label="Disconnect Current Server", command=...)
+FileMenuBar.add_command(label="New Connection", command=...)
+FileMenuBar.add_separator()
+FileMenuBar.add_command(label="Exit", command=...)
+WindowMenu.add_cascade(label="File", menu=FileMenuBar)
+
+SettingMenuBar = Menu(WindowMenu, tearoff=0)
+SettingMenuBar.add_command(label="User Setting", command=...)
+SettingMenuBar.add_command(label="Connection Setting", command=...)
+SettingMenuBar.add_command(label="Interface Setting", command=...)
+WindowMenu.add_cascade(label="Setting", menu=SettingMenuBar)
+window.config(menu=WindowMenu)
+
+client_id = 'Client-' + ''.join(rd.choices(string.ascii_uppercase + string.digits, k=9))
+client = mqtt.Client(client_id)
+
+client.on_connect = on_connection
+client.on_message = on_message
+
+client.connect(hostname, 1883)
+client.loop_start()
+window.mainloop()
